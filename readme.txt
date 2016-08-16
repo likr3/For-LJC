b54b426463c0a62ce372180a905214f3b4d03a42
likr3 <154426812@qq.com>
2016-08-16 17:34:31 +0800

    new update for LJC

---
+++ readme.txt
@@ -0,0 +1,1 @@
+wget is used to downlaod.
---
+++ remove.py
@@ -0,0 +1,18 @@
+from PIL import Image
+currDir = 'C:\Users\liker\PycharmProjects\untitled'
+
+import os
+def removeFile(dir,postfix):
+    if os.path.isdir(dir):
+        for file in os.listdir(dir):
+            removeFile(dir+'/'+file,postfix)
+    else:
+        if os.path.splitext(dir)[1] == postfix:
+            os.remove(dir)
+
+
+
+
+
+
+removeFile(currDir,'.jpg')
